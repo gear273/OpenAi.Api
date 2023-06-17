177a2fedef5db24e13bbc0b221452ac2f9bb3e3e
connorbowlan <connor@cbowlan.com>
2023-06-17 15:17:44 -0700

    Add project files.

---
+++ OpenAiCompletion/CompletionProvider.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using OpenAiCompletion.Models;
+
+namespace OpenAiCompletion;
+
+/// <summary>
+/// Provides a method to generate completions using an HTTP client.
+/// </summary>
+public class CompletionProvider
+{
+    private readonly HttpClient _client;
+
+    /// <summary>
+    /// Initializes a new instance of the CompletionProvider class with the specified HTTP client.
+    /// </summary>
+    /// <param name="client">The HttpClient used for making API requests.</param>
+    public CompletionProvider(HttpClient client)
+    {
+        _client = client;
+    }
+
+    /// <summary>
+    /// Generates a completion asynchronously using the specified completion request.
+    /// </summary>
+    /// <param name="completionRequest">The CompletionRequest object containing the completion parameters.</param>
+    /// <returns>A string representing the generated completion response in JSON format.</returns>
+    public async Task<string?> GenerateCompletionAsync(CompletionRequest completionRequest)
+    {
+        try
+        {
+            var request = new StringContent(JsonConvert.SerializeObject(completionRequest), Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("https://api.openai.com/v1/engines/davinci-codex/completions", request);
+
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            return json;
+        }
+        catch
+        {
+            return default;
+        }
+    }
+}
---
+++ OpenAiCompletion/Models/CompletionRequest.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+
+namespace OpenAiCompletion.Models;
+
+/// <summary>
+/// Represents a completion request for an AI model.
+/// </summary>
+public class CompletionRequest
+{
+    /// <summary>
+    /// Initializes a new instance of the CompletionRequest class with the specified prompt.
+    /// </summary>
+    /// <param name="prompt">The prompt text for the completion request.</param>
+    public CompletionRequest(string prompt)
+    {
+        Prompt = prompt;
+    }
+
+    /// <summary>
+    /// Gets or sets the model to use for the completion request.
+    /// </summary>
+    [JsonProperty("model")]
+    public string Model { get; set; } = "text-davinci-003";
+
+    /// <summary>
+    /// Gets or sets the prompt text for the completion request.
+    /// </summary>
+    [JsonProperty("prompt")]
+    public string Prompt { get; set; }
+
+    /// <summary>
+    /// Gets or sets an optional suffix to append to the completion result.
+    /// </summary>
+    [JsonProperty("suffix")]
+    public string? Suffix { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum number of tokens to generate in the completion response.
+    /// </summary>
+    [JsonProperty("max_tokens")]
+    public int? MaxTokens { get; set; } = 16;
+
+    /// <summary>
+    /// Gets or sets the temperature value for randomness in the completion generation.
+    /// </summary>
+    [JsonProperty("temperature")]
+    public double Temperature { get; set; } = 1;
+
+    /// <summary>
+    /// Gets or sets the top-p value for nucleus sampling in the completion generation.
+    /// </summary>
+    [JsonProperty("top_p")]
+    public double TopP { get; set; } = 1;
+
+    /// <summary>
+    /// Gets or sets the number of completions to generate.
+    /// </summary>
+    [JsonProperty("n")]
+    public int N { get; set; } = 1;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the completions should be streamed as they become available.
+    /// </summary>
+    [JsonProperty("stream")]
+    public bool Stream { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets the number of log probabilities to include in the completion response.
+    /// </summary>
+    [JsonProperty("logprobs")]
+    public int? LogProbs { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether to include the prompt in the completion response.
+    /// </summary>
+    [JsonProperty("echo")]
+    public bool Echo { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets a list of strings that serve as stopping conditions for the completion generation.
+    /// </summary>
+    [JsonProperty("stop")]
+    public List<string>? Stop { get; set; }
+
+    /// <summary>
+    /// Gets or sets the presence penalty value for encouraging the model to be more cautious in generating repetitive completions.
+    /// </summary>
+    [JsonProperty("presence_penalty")]
+    public double PresencePenalty { get; set; } = 0;
+
+    /// <summary>
+    /// Gets or sets the frequency penalty value for discouraging the model from generating repetitive completions.
+    /// </summary>
+    [JsonProperty("frequency_penalty")]
+    public double FrequencyPenalty { get; set; } = 0;
+
+    /// <summary>
+    /// Gets or sets the number of candidates to sample and rank in the completion generation.
+    /// </summary>
+    [JsonProperty("best_of")]
+    public int? BestOf { get; set; } = 1;
+
+    /// <summary>
+    /// Gets or sets a dictionary representing the logit bias to influence the model's output probabilities.
+    /// </summary>
+    [JsonProperty("logit_bias")]
+    public Dictionary<int, double>? LogitBias { get; set; }
+
+    /// <summary>
+    /// Gets or sets the user identifier associated with the completion request.
+    /// </summary>
+    [JsonProperty("user")]
+    public string? User { get; set; }
+}
